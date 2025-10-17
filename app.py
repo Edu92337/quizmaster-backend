496449a248fe09e66d0a60b6ba72547f4ef59ad6
QuizMaster Developer <quizmaster@example.com>
2025-10-17 16:43:14 -0400

    Add gunicorn, flask-cors and update app.py for production

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Configuração do Banco de Dados
 app.config.from_object(Config)
