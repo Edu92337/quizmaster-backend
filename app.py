552bcfcffb40e263ad7c9c4dae851d267ec6f56f
Eduardo Augusto Almeida lira <eduz92337@gmail.com>
2025-10-17 19:22:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from .config import Config
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
